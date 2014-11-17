722efc983ca7ccbc0f116f25131cdad9559c3b6b
Michael Favila <resourcemode@yahoo.com>
2014-11-17 16:35:33 +0800

    Update FizzBuzzTest.php

--- Test/FizzBuzzTest.php
+++ Test/FizzBuzzTest.php
@@ -1,11 +1,14 @@
+<?php
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
     protected $number = 100;
 	protected $fizzBuzz;
+
     public function setUp()
     {
         $this->fizzBuzz = new FizzBuzz($this->number);
     }
+
     public function testIsNumber()
     {
 	    if (is_numeric($this->number)) {
@@ -18,14 +21,17 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
             $this->assertEquals($expected, $actual);
 		}
     }
+
 	public function testBuzz()
 	{
 		$this->assertEquals('Buzz', FizzBuzz::getResult()[5]);
 	}
+
 	public function testFizz()
 	{
 		$this->assertEquals('Fizz', FizzBuzz::getResult()[3]);
 	}
+
 	public function testFizzBuzz()
 	{
 		$this->assertEquals('FizzBuzz', FizzBuzz::getResult()[15]);
