374516ed625eb47853737e2844e4f34f5dd67463
Michael Favila <resourcemode@yahoo.com>
2014-11-17 16:55:51 +0800

    Update FizzBuzzTest.php

--- Test/FizzBuzzTest.php
+++ Test/FizzBuzzTest.php
@@ -13,13 +13,28 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
     public function testIsNumber()
     {
 	    if (is_numeric($this->number)) {
-		    $expected = $this->number;
+            $expected = $this->number;
             $actual = FizzBuzz::isNumber($this->number);
             $this->assertEquals($expected, $actual);
 		} else {
-		    $expected = 'Parameter is not a number';
+            $expected = 'Parameter is not a number';
             $actual = FizzBuzz::isNumber($param);
             $this->assertEquals($expected, $actual);
 		}
     }
+
+	public function testBuzz()
+	{
+		$this->assertEquals('Buzz', FizzBuzz::getResult()[5]);
+	}
+
+	public function testFizz()
+	{
+		$this->assertEquals('Fizz', FizzBuzz::getResult()[3]);
+	}
+
+	public function testFizzBuzz()
+	{
+		$this->assertEquals('FizzBuzz', FizzBuzz::getResult()[15]);
+	}
 }
