685f1fb273577b6e62328b2b8c80a38c31e041d8
Michael Favila <resourcemode@yahoo.com>
2014-11-20 15:06:13 +0800

    Update FizzBuzz.php

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -10,7 +10,7 @@ class FizzBuzz
 	    $number = self::isNumber($param);
 		if ($number > 1 && $number <= 100) {
 		    $results = array();
-		    for($i=1; $i<=$number; $i++) {
+		    for($i = 1; $i <= $number; $i++) {
 		        if ($i % 3 == 0 && $i % 5 == 0) {
 				    $results[$i] = self::getFizzBuzz();		        
 			    } elseif ($i % 5 == 0) {
